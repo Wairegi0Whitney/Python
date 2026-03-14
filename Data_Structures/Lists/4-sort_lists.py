eb934739b7e1f1784b55fc46971bf0697b8388fd
Wairegi <wairegiwhitney11@gmail.com>
2026-03-15 00:59:33 +0300

    15/3 lists

---
+++ Data_Structures/Lists/4-sort_lists.py
@@ -0,0 +1,8 @@
+MyList=["nairobi", "kisumu","murang'a", "garissa", "baringo"]
+#Sort the list alphabetically:
+MyList.sort()
+print(MyList)
+
+#To sort descending, use the keyword argument reverse = True:
+MyList.sort(reverse=True)
+print(MyList)
